023fc8c83ef45d82a57c2c66332433ff52f42587
kenanboracic4 <kenanboracic.dev@gmail.com>
2025-12-10 22:54:43 +0100

    Selection Sort

--- sorts/selectionSort/selectionSort.cpp
+++ sorts/selectionSort/selectionSort.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 void selectionSort(vector<int> &v) {
 
-   for (int i=0; i<v.size()-1; i++) {
+   for (int i = 0; i<v.size()-1;i++) {
        int idx = i;
-       for (int j = i+1;j<v.size();j++) {
+       for (int j = i + 1; j <v.size(); j++) {
            if (v[j] < v[idx])
                idx = j;
        }
