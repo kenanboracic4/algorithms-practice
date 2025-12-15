07f6aabb06151d931f0e17423551a32c5aa77e56
kenanboracic4 <kenanboracic.dev@gmail.com>
2025-12-15 18:56:27 +0100

    Upgrade of upgrade List

---
+++ structures/list/list.cpp
@@ -0,0 +1,181 @@
+#include <iostream>
+
+using namespace std;
+
+template<typename T>
+class Lista {
+    class Cvor {
+    public:
+        T vrijednost;
+        Cvor* sljedeci;
+
+        Cvor(T v, Cvor* next = nullptr):vrijednost(v), sljedeci(next){}
+        ~Cvor(){}
+    };
+
+    Cvor* glava;
+
+public:
+    Lista():glava(nullptr){}
+    ~Lista() {
+        auto tekuci = glava;
+        while (tekuci != nullptr) {
+            auto temp = tekuci->sljedeci;
+            delete tekuci;
+            tekuci = temp;
+        }
+    }
+
+    void push_front(const T vrijednost) {
+        if (glava == nullptr) {
+            auto noviCvor = new Cvor(vrijednost);
+            glava = noviCvor;
+        }else {
+            auto noviCvor = new Cvor(vrijednost, glava);
+            glava = noviCvor;
+        }
+    }
+
+    void push_back(const T vrijednost) {
+        if (glava == nullptr) {
+            auto noviCvor = new Cvor(vrijednost);
+            glava = noviCvor;
+        }else {
+            auto tekuci = glava;
+            auto noviCvor = new Cvor(vrijednost);
+
+            while (tekuci ->sljedeci != nullptr) {
+                tekuci = tekuci->sljedeci;
+            }
+            tekuci->sljedeci = noviCvor;
+
+
+        }
+    }
+
+    void obrisi(const T vrijednost) {
+        if (glava == nullptr) return;
+
+        auto tekuci = glava;
+        auto pokNaPrethodni = &glava;
+
+        while (tekuci != nullptr) {
+            if (tekuci ->vrijednost == vrijednost) {
+                *pokNaPrethodni = tekuci->sljedeci;
+                delete tekuci;
+                return;
+            }
+            pokNaPrethodni = &tekuci->sljedeci;
+            tekuci = tekuci->sljedeci;
+        }
+
+    }
+
+    void deleteAtIndex(int index) {
+        if (index < 0 ) return;
+        if (index == 0) {
+            if (nullptr != glava) {
+                auto sljedeci = glava ->sljedeci;
+                delete glava;
+                glava = sljedeci;
+            }
+            return;
+        }
+
+        auto tekuci = glava;
+        for (int i = 0; i < index - 1; i++) {
+            if (tekuci->sljedeci == nullptr) {
+                std::cout << "Greška: Indeks je izvan opsega liste." << std::endl;
+                return;
+            }
+            tekuci = tekuci->sljedeci;
+        }
+
+        if (tekuci->sljedeci == nullptr) {
+            std::cout << "Greška: Nema čvora na tom indeksu za brisanje." << std::endl;
+            return;
+        }
+
+        auto prethodnik = tekuci;
+        auto cvorZaBrisanje = tekuci->sljedeci;
+        auto sljedecii = cvorZaBrisanje ->sljedeci;
+        prethodnik ->sljedeci = sljedecii;
+        delete cvorZaBrisanje;
+    }
+
+    void insertAtIndex(const T vrijednost, int index) {
+
+        if (index == 0) {
+            auto noviCvor = new Cvor(vrijednost, glava);
+            glava = noviCvor;
+        }else {
+            auto tekuci = glava;
+            for (int i = 0; i<index-1; i++) {
+                tekuci = tekuci -> sljedeci;
+            }
+            auto prethodni = tekuci ;
+            auto trenutni = tekuci ->sljedeci;
+            auto noviCvor = new Cvor(vrijednost,trenutni);
+            prethodni->sljedeci = noviCvor;
+
+        }
+    }
+
+    void obrtanje() {
+        Cvor* prethodni = nullptr;
+        auto trenutni = glava;
+        Cvor* sljedeci = nullptr;
+
+        while (trenutni != nullptr) {
+            sljedeci = trenutni ->sljedeci;
+            trenutni -> sljedeci = prethodni;
+            prethodni = trenutni;
+            trenutni = sljedeci;
+        }
+        glava = prethodni;
+    }
+
+    void ispisi() {
+        auto tekuci = glava;
+
+        while (tekuci != nullptr) {
+            cout<<tekuci->vrijednost<<" ";
+            tekuci = tekuci->sljedeci;
+        }
+        cout<<endl;
+    }
+
+    class IteratorListe {
+    public:
+        Cvor* i_cvor;
+
+        IteratorListe(Cvor* i):i_cvor(i){}
+        IteratorListe& operator++() {
+            if (i_cvor != nullptr) i_cvor = i_cvor->sljedeci;
+            return *this;
+        }
+        T& operator*(){return i_cvor->vrijednost; }
+        bool operator != (const IteratorListe i) {
+            return i_cvor != i.i_cvor;
+        }
+    };
+};
+
+int main() {
+
+    Lista<int> l;
+    l.push_front(1);
+    l.push_front(2);
+    l.push_back(5);
+    l.push_back(10);
+
+
+
+    l.ispisi();
+    l.insertAtIndex(3,2);
+    l.ispisi();
+    l.obrtanje();
+    l.ispisi();
+
+    return 0;
+}
